cae615d2b8e3b37a10517848122c6e8cc5333c38
Maxim Vintsyun <maxes2000@yandex.ru>
2023-07-22 16:36:48 +0500

    fix: рефакторинг

--- scripts/script.js
+++ scripts/script.js
@@ -6,23 +6,10 @@ import PopupWithForm from "./PopupWithForm.js";
 import UserInfo from "./UserInfo.js";
 
 const profileEditButton = document.querySelector('.profile__edit-button');
-const profileName = document.querySelector('.profile__name');
-const profileAbout = document.querySelector('.profile__about');
 const profileAddCard = document.querySelector('.profile__add-button');
 
-const popupList = document.querySelectorAll('.popup');
-
-const popupProfile = document.querySelector('#popup__profile');
-const popupProfileContainer = document.querySelector('#popup__container_profile');
 const nameInput = document.querySelector('#profile-name-input');
 const aboutInput = document.querySelector('#profile-about-input');
-
-const popupAddCard = document.querySelector('#popup__add-card');
-const popupAddCardContainer = document.querySelector('#popup__container_add-card');
-const cardNameInput = document.querySelector('#place-name-input');
-const cardLinkInput = document.querySelector('#place-link-input');
-
-const closeButtonList = document.querySelectorAll('.popup__close-button');
 
 const userInfo = new UserInfo({
     nameSelector: '.profile__name', 
@@ -35,9 +22,6 @@ const profilePopup = new PopupWithForm(
         evt.preventDefault();
 
         userInfo.setUserInfo(data['profile-name'], data['profile-about']);
-    
-        // profileName.textContent = data['profile-name'];
-        // profileAbout.textContent = data['profile-about'];
     
         profilePopup.close();
     }
@@ -55,8 +39,6 @@ const addCardPopup = new PopupWithForm(
         };
     
         renderCards.prependItem(createCard(cardData));
-    
-        // evt.target.reset();
     
         addCardPopup.close();
     }
@@ -100,32 +82,6 @@ const renderCards = new Section({
 
 renderCards.renderItems();
 
-function closePopup(popup) {
-    popup.classList.remove('popup_opened');
-    document.removeEventListener('keydown', escPopupClosing);
-}
-
-// function clickOutPopupClosing(evt) {
-//     if(evt.currentTarget === evt.target) { 
-//         closePopup(evt.currentTarget); 
-//     } 
-// }
-
-// popupList.forEach(popup => {
-//     popup.addEventListener('click', clickOutPopupClosing);
-// })
-
-function escPopupClosing(evt) {
-    if(evt.key === 'Escape') {
-        closePopup(document.querySelector('.popup_opened'));
-    }
-}
-
-// function openPopup(popup) {
-//     popup.classList.add('popup_opened');
-//     document.addEventListener('keydown', escPopupClosing);
-// }
-
 profileEditButton.addEventListener('click', () => {
     formValidators['popup-form-profile'].resetValidation();
     const userData = userInfo.getUserInfo();
@@ -139,38 +95,3 @@ profileAddCard.addEventListener('click', () => {
     formValidators['popup-form-card'].resetValidation();
     addCardPopup.open();
 });
-
-// closeButtonList.forEach(closeButton => {
-//     closeButton.addEventListener('click', evt => closePopup(evt.target.closest('.popup')));
-// })
-
-// function addCard(evt, data) {
-//     evt.preventDefault();
-    
-
-//     const cardData = {
-//         name: data['place-name'],
-//         link: data['place-link']
-//     };
-
-//     renderCards.prependItem(createCard(cardData));
-
-//     // evt.target.reset();
-
-//     addCardPopup.close();
-// }
-
-// popupAddCardContainer.addEventListener('submit', evt => addCard(evt));
-
-// function changeProfileData(evt, data) {
-//     evt.preventDefault();
-    
-//     console.log(data);
-
-//     profileName.textContent = data['profile-name'];
-//     profileAbout.textContent = data['profile-about'];
-
-//     profilePopup.close();
-// }
-
-// popupProfileContainer.addEventListener('submit', evt => changeProfileData(evt));
